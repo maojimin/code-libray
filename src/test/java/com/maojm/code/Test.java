aa456b276762bf6b0254f87212d941bcd6acba29
maojimin <maojimin8888@163.com>
2014-08-01 23:30:00 +0800

    提交一下

---
+++ src/test/java/com/maojm/code/Test.java
@@ -0,0 +1,14 @@
+/**
+ * yutian.com.cn Inc.
+ * Copyright (c) 宇天科技股份有限公司 2010-2014 All Rights Reserved.
+ */
+package com.maojm.code;
+
+/**
+ * @author <a href="mailto:maojimin@yutian.com.cn">毛积敏</a>
+ * 2014-8-1 - 下午11:29:04
+ * @since 1.0
+ */
+public class Test {
+
+}
